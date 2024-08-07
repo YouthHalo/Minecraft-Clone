02ce150a8cf51bcc9876d736484037acb2f40d9a
YouthHalo <69648246+YouthHalo@users.noreply.github.com>
2024-08-07 16:14:55 -0400

    Slightly improved breaking and building raycast
    Doesn't mean it works 100% of the time though :/

--- scenes/player.gd
+++ scenes/player.gd
@@ -10,9 +10,9 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 var rot_x = 0
 var rot_y = 0
-
 func _input(event):
-	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	##Input.warp_mouse(Vector2(get_viewport().get_visible_rect().size.x / 2, get_viewport().get_visible_rect().size.x / 2))
 	if event is InputEventMouseMotion:
 		# modify accumulated mouse rotation
 		rot_x += -(event.relative.x * 0.01)
@@ -23,26 +23,40 @@ func _input(event):
 		rotate_object_local(Vector3(0, 1, 0), rot_x) # rotate player in Y
 		
 		$Camera3D.rotate_object_local(Vector3(1, 0, 0), rot_y) # then rotate in X for camera
+
+	var space_state = get_world_3d().direct_space_state
+	var cam = $Camera3D
+	var mousepos = get_viewport().get_mouse_position()
+	var origin = cam.project_ray_origin(mousepos)
+	var end = origin + cam.project_ray_normal(mousepos) * 20000
+	var query = PhysicsRayQueryParameters3D.create(origin, end, collision_mask, [self])
+	query.collide_with_areas = true
+
+	var result = space_state.intersect_ray(query)
+	
 	if Input.is_action_just_pressed("leftClick"):
-		print("CLICK")
-
-		var space_state = get_world_3d().direct_space_state
-		var cam = $Camera3D
-		var mousepos = get_viewport().get_mouse_position()
-
-		var origin = cam.project_ray_origin(mousepos)
-		var end = origin + cam.project_ray_normal(mousepos) * 50000
-		var query = PhysicsRayQueryParameters3D.create(origin, end)
-		query.collide_with_areas = true
-
-		var result = space_state.intersect_ray(query)
-		print(result)
-		print($"../GridMap".get_cell_item(result.position))
-		$"../GridMap".set_cell_item(result.position, 0)
 		
+		print(result)
+		print($"../GridMap".local_to_map(result.position))
+		print($"../GridMap".get_cell_item(result.position))
+		if result.has("position"):
+			$"../GridMap".set_cell_item($"../GridMap".local_to_map(result.position), (-1))
+			$"../GridMap".set_cell_item(result.position, (-1))
+			$"../GridMap".set_cell_item(result.normal, (-1))
+		
+		
+	if Input.is_action_just_pressed("rightClick"):
+		
+		print(result)
+		if result.has("position"):
+			print($"../GridMap".get_cell_item(result.position))
+			$"../GridMap".set_cell_item(result.position, 0)
+	
 		
 
 func _physics_process(delta):
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	Input.warp_mouse(Vector2(get_viewport().get_visible_rect().size.x / 2, get_viewport().get_visible_rect().size.y / 2))
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
