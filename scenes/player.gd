24f850a659e904106b7ce0c8c07b5d4778add6cd
YouthHalo <69648246+YouthHalo@users.noreply.github.com>
2024-08-07 12:32:39 -0400

    Mouse looking, gridmap blocks, wasd moving, cursor

--- scenes/player.gd
+++ scenes/player.gd
@@ -8,6 +8,22 @@ const JUMP_VELOCITY = 4.5
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
+var rot_x = 0
+var rot_y = 0
+
+func _input(event):
+	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
+	if event is InputEventMouseMotion:
+		# modify accumulated mouse rotation
+		rot_x += -(event.relative.x * 0.02)
+		rot_y += -(event.relative.y * 0.02)
+		$Camera3D.transform.basis = Basis() # reset rotation
+		transform.basis = Basis()
+		##$Camera3D.rotate_object_local(Vector3(0, 1, 0), rot_x) # first rotate camera in Y
+		rotate_object_local(Vector3(0, 1, 0), rot_x) # Also rotate the Player
+		$Camera3D.rotate_object_local(Vector3(1, 0, 0), rot_y) # then rotate in X
+		
+
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -28,5 +44,5 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	
 	move_and_slide()
