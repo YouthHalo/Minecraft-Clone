266a56d049b3f6cd4952eba9eb1191e2e1f72022
YouthHalo <69648246+YouthHalo@users.noreply.github.com>
2024-08-07 13:30:11 -0400

    Building Blocks added, Needs a fixed raycast
    Currently blocks are being placed in seemingly random places, and crashes the game if no block was found to raycast off of. Needs fixing ASAP

--- scenes/player.gd
+++ scenes/player.gd
@@ -15,20 +15,37 @@ func _input(event):
 	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 	if event is InputEventMouseMotion:
 		# modify accumulated mouse rotation
-		rot_x += -(event.relative.x * 0.02)
-		rot_y += -(event.relative.y * 0.02)
+		rot_x += -(event.relative.x * 0.01)
+		rot_y += -(event.relative.y * 0.01)
+		
 		$Camera3D.transform.basis = Basis() # reset rotation
 		transform.basis = Basis()
-		##$Camera3D.rotate_object_local(Vector3(0, 1, 0), rot_x) # first rotate camera in Y
-		rotate_object_local(Vector3(0, 1, 0), rot_x) # Also rotate the Player
-		$Camera3D.rotate_object_local(Vector3(1, 0, 0), rot_y) # then rotate in X
+		rotate_object_local(Vector3(0, 1, 0), rot_x) # rotate player in Y
+		
+		$Camera3D.rotate_object_local(Vector3(1, 0, 0), rot_y) # then rotate in X for camera
+	if Input.is_action_just_pressed("leftClick"):
+		print("CLICK")
+
+		var space_state = get_world_3d().direct_space_state
+		var cam = $Camera3D
+		var mousepos = get_viewport().get_mouse_position()
+
+		var origin = cam.project_ray_origin(mousepos)
+		var end = origin + cam.project_ray_normal(mousepos) * 50000
+		var query = PhysicsRayQueryParameters3D.create(origin, end)
+		query.collide_with_areas = true
+
+		var result = space_state.intersect_ray(query)
+		print(result)
+		print($"../GridMap".get_cell_item(result.position))
+		$"../GridMap".set_cell_item(result.position, 0)
+		
 		
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-		print("Falling")
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
