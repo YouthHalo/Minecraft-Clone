0a2642338adfc9f3f877886518eb0d4f943d4f51
YouthHalo <69648246+YouthHalo@users.noreply.github.com>
2024-08-07 22:16:14 -0400

    Semi-functional crouching + textured block!

--- scenes/player.gd
+++ scenes/player.gd
@@ -3,12 +3,15 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var mouse_sens = 0.005
+var speedmulti = 0
 var rot_x = 0
 var rot_y = 0
 var blockID = 0
+
 @onready var raycast = $Camera3D/RayCast3D
 @onready var camera = $Camera3D
 
@@ -31,18 +34,30 @@ func movement(delta):
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * SPEED * speedmulti
+		velocity.z = direction.z * SPEED * speedmulti
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+	
+	if Input.is_action_just_pressed("crouch"):
+		camera.position.y = 0.495
+		speedmulti = 0.3
+	if Input.is_action_just_released("crouch"):
+		camera.position.y = 0.62
+		speedmulti = 1
+		
+	
 
 
 func _input(event):
+	
+	if position.y < -20
+		get_tree().quit()
 	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		# modify accumulated mouse rotation
-		rot_x += -(event.relative.x * 0.01)
-		rot_y += -(event.relative.y * 0.01)
+		rot_x += -(event.relative.x * mouse_sens)
+		rot_y += -(event.relative.y * mouse_sens)
 		
 		camera.transform.basis = Basis() # reset rotation
 		transform.basis = Basis()
