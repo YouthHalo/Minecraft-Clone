10f532a1a5a0c33ea2a09cfd6a621bbecfd6e991
YouthHalo <69648246+YouthHalo@users.noreply.github.com>
2024-08-11 09:07:10 -0400

    Coordiante HUD, adding stairs

--- scenes/ui.gd
+++ scenes/ui.gd
@@ -1,6 +1,6 @@
 extends Control
 
-
+@onready var player = $"../../../.."
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -10,3 +10,4 @@ func _ready():
 func _process(delta):
 	$"FPS Counter".text = "FPS: " + str(Engine.get_frames_per_second())
 	$"Frame Time".text = "Frame Time: " + str(round(delta*100000)/100) + "ms"
+	$Coordinates.text = "(" + str(round(player.position.x * 100) / 100) + ", " + str(round(player.position.y * 100) / 100 + 0.06) + ", " + str(round(player.position.z * 100) / 100) + ")"
