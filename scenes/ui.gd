fa7740089c893809abf44a1d6e9a50c86bf9177f
YouthHalo <69648246+YouthHalo@users.noreply.github.com>
2024-08-13 10:35:00 -0400

    Better Pause menu with Exit game

--- scenes/ui.gd
+++ scenes/ui.gd
@@ -1,12 +1,17 @@
 extends Control
 
 @onready var player = $"../../../.."
+@onready var pauseMenu = $"Pause Menu"
+@onready var inventory = $Inventory
 # Called when the node enters the scene tree for the first time.
 
 var paused = false
+var inInventory = false
 
 func _ready():
-	pass # Replace with function body.
+	pauseMenu.hide()
+	inventory.hide()
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -17,14 +22,26 @@ func _process(delta):
 	if Input.is_action_just_pressed("pause"):
 		if not paused:
 			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+			pauseMenu.show()
 			get_tree().paused = true
 			paused = true
 		else:
 			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+			pauseMenu.hide()
 			get_tree().paused = false
 			paused = false
-	if paused:
-		$"Pause Menu".show()
-	else:
-		$"Pause Menu".hide()
+
+	if Input.is_action_just_pressed("inventory"):
+		if not inInventory:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+			inventory.show()
+			inInventory = true
+		else:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+			inventory.hide()
+			inInventory = false
 	
+
+
+func _on_quit_pressed():
+	get_tree().quit()
