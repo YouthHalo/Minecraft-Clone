fc7bc3bfd781d96ac114876d042c51ddf64e5df8
YouthHalo <69648246+YouthHalo@users.noreply.github.com>
2024-08-13 21:36:15 -0400

    Title screen music, Splash text in works

--- scenes/title_screen.gd
+++ scenes/title_screen.gd
@@ -1,5 +1,9 @@
 extends Control
 
+@onready var splash = $SplashText
+
+var grow = true
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -7,8 +11,19 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if splash.scale < Vector2(1.2, 1.2) and grow:
+		if splash.scale == Vector2(1.2, 1.2):
+			print("GROWSTOP")
+			grow = false
 
+		splash.scale += Vector2(0.01, 0.01)
+		print(splash.scale)
+
+	if splash.scale > Vector2(0.75, 0.75) and not grow:
+		splash.scale -= Vector2(0.05, 0.05)
+		if splash.scale == Vector2(0.8, 0.8):
+			grow = true
+			print("GROWSTART")
 
 func _on_quit_pressed():
 	get_tree().quit()
