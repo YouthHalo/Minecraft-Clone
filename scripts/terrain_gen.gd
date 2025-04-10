f2c5e181dc09a0ca2f56d7bfc90ed52701ec129c
YouthHalo <69648246+YouthHalo@users.noreply.github.com>
2025-04-10 18:04:45 -0400

    updated godot version

--- scripts/terrain_gen.gd
+++ scripts/terrain_gen.gd
@@ -2,11 +2,10 @@ extends Node3D
 
 var stone = preload("res://scenes/blocks/stone.tscn")
 var dirt = preload("res://scenes/blocks/dirt.tscn")
-var ore = preload("res://scenes/blocks/diamond_ore.tscn")
 @onready var camera = $TestCamera/Camera3D
 # Called when the node enters the scene tree for the first time.
 
-var plane_size = 128
+var plane_size = 64
 var block_size = 1.0
 var mouseSens = 0.005
 var Y = 0
@@ -29,28 +28,15 @@ func _input(event):
 func generate_planes() -> void:
 	for x in plane_size:
 		for z in plane_size:
-			var ore_instance: Node3D = ore.instantiate()
-			ore_instance.transform.origin = Vector3(1 * block_size, Y, 1 * block_size)
-			add_child(ore_instance)
 			var stone_instance: Node3D = stone.instantiate()
-			stone_instance.transform.origin = Vector3(1 * block_size, Y+1, 1 * block_size)
+			stone_instance.transform.origin = Vector3(z * block_size, Y+1, x * block_size)
 			add_child(stone_instance)
 			# Generate dirt blocks at y=1
 			var dirt_instance: Node3D = dirt.instantiate()
-			dirt_instance.transform.origin = Vector3(1 * block_size, Y+2, 1 * block_size)
+			dirt_instance.transform.origin = Vector3(z * block_size, Y+2, x * block_size)
 			add_child(dirt_instance)
-	Y += 3
+	Y += 2
 
 
 func generate_planes2() -> void:
-	var ore_instance: Node3D = ore.instantiate()
-	ore_instance.transform.origin = Vector3(1 * block_size, Y, 1 * block_size)
-	add_child(ore_instance)
-	var stone_instance: Node3D = stone.instantiate()
-	stone_instance.transform.origin = Vector3(1 * block_size, Y+1, 1 * block_size)
-	add_child(stone_instance)
-	# Generate dirt blocks at y=1
-	var dirt_instance: Node3D = dirt.instantiate()
-	dirt_instance.transform.origin = Vector3(1 * block_size, Y+2, 1 * block_size)
-	add_child(dirt_instance)
-	Y += 3
+	generate_planes()
